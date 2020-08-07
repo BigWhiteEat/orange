46f6322c48d911f14ea59039082fd84c1bd7f4d6
修罗厉 <703351566@qq.com>
2020-08-08 02:15:45 +0800

    Updates

---
+++ precache-manifest.f328c1fe833a39af0ea5ce93c7a7402a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9e1ddc6ef62b39f77f0d5f30e07679",
+    "url": "/orange/index.html"
+  },
+  {
+    "revision": "d196955a57884368c651",
+    "url": "/orange/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "f971126b93fc06db9d39",
+    "url": "/orange/static/css/main.3df5a832.chunk.css"
+  },
+  {
+    "revision": "d196955a57884368c651",
+    "url": "/orange/static/js/2.ad2835c0.chunk.js"
+  },
+  {
+    "revision": "6e8ee2f1abb1c07618978a09f2d8a69d",
+    "url": "/orange/static/js/2.ad2835c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f971126b93fc06db9d39",
+    "url": "/orange/static/js/main.b6769954.chunk.js"
+  },
+  {
+    "revision": "f2bb4cbff17ca48e89ed",
+    "url": "/orange/static/js/runtime-main.df1ff0be.js"
+  }
+]);

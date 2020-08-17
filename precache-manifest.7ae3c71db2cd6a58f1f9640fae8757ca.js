60fb42e3b20327176bc7daefb7f68a42be39be14
修罗厉 <703351566@qq.com>
2020-08-17 16:20:41 +0800

    Updates

---
+++ precache-manifest.7ae3c71db2cd6a58f1f9640fae8757ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08dea8342ced7b8d2d8481d69c6f812",
+    "url": "/orange/index.html"
+  },
+  {
+    "revision": "e6c846ee908a5da3a9b6",
+    "url": "/orange/static/css/2.ae66d788.chunk.css"
+  },
+  {
+    "revision": "91bbf9091b109395ab9b",
+    "url": "/orange/static/css/main.79144e10.chunk.css"
+  },
+  {
+    "revision": "e6c846ee908a5da3a9b6",
+    "url": "/orange/static/js/2.bcefb333.chunk.js"
+  },
+  {
+    "revision": "2393e110b15940b27f39a74b3a4f99b4",
+    "url": "/orange/static/js/2.bcefb333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bbf9091b109395ab9b",
+    "url": "/orange/static/js/main.dbaad504.chunk.js"
+  },
+  {
+    "revision": "b8f8aec124594026b0e4",
+    "url": "/orange/static/js/runtime-main.8d6719b3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/orange/precache-manifest.da03bfc90d164e17f9ab46c2e60564a7.js"
+  "/orange/precache-manifest.7ae3c71db2cd6a58f1f9640fae8757ca.js"
 );
 
 self.addEventListener('message', (event) => {
